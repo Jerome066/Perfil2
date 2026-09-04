ca17e51f0ee3029544ba405036137fb8c924432e
Jorge T <jor.tec.vill@gmail.com>
2026-09-04 10:22:32 -0600

    Añade la nueva tab en el menu

--- src/app/components/json-tabs/json-tabs.component.ts
+++ src/app/components/json-tabs/json-tabs.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, input, output } from '@angular/core';
 import {MatTabChangeEvent, MatTabsModule} from '@angular/material/tabs';
 import { JsonTabs } from '../../models/json-tabs';
 import { JsonServiceService } from '../../services/json-service.service';
+import { JsonInfo } from '../../models/json-info';
 
 
 @Component({
@@ -14,6 +15,7 @@ export class JsonTabsComponent {
   private datosArbol = inject(JsonServiceService);
   tabs = input.required<JsonTabs[]>();
   tabActual = output <number>();
+  extraTab = input.required<JsonInfo[]>();
 
   onTabChange(event: MatTabChangeEvent): void {
     this.tabActual.emit(event.index);
